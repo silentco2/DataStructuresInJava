3e3df8e56a822d08cfc6a5857df78aa22f98d3c2
silentco2 <92058267+silentco2@users.noreply.github.com>
2022-12-23 13:28:55 +0200

    Create HeapSort.java

---
+++ HeapSort.java
@@ -0,0 +1,31 @@
+public class HeapSort {
+    int[] arr;
+    Sort(int[] a){arr = a;}
+  
+    public void Heap(){
+        buildHeap();
+        for(int i=arr.length-1;i>=0;i--){
+            swap(0,i);
+            heapify(0,i);
+        }
+    }
+  
+    private void buildHeap(){for (int i=arr.length/2-1;i>=0;i--) heapify(i,arr.length);}
+  
+    private void heapify(int root,int size){
+        int left = root*2+1,right = left+1;
+        int max = root;
+        if (left<size && arr[left]>arr[root]) max = left;
+        if (right<size && arr[right]>arr[root]) max = right;
+        if (max != root){
+            swap(root,max);
+            heapify(max,size);
+        }
+    }
+  
+    private void swap(int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
